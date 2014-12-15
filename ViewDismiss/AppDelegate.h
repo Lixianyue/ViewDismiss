dea05ce0d6757947fca3376cedb339b22c49fcac
msd <msd@lxy.local>
2014-12-15 16:15:26 +0800

    Initial Commit

---
+++ ViewDismiss/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewDismiss
+//
+//  Created by msd on 14-12-15.
+//  Copyright (c) 2014年 msd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewDismiss/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViewDismiss
+//
+//  Created by msd on 14-12-15.
+//  Copyright (c) 2014年 msd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
